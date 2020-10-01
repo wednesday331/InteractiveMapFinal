0a08e0c89265ee681a32efcc7468b9d4b31136f0
wednesday331 <65116875+wednesday331@users.noreply.github.com>
2020-10-01 09:45:32 -0400

    PEP8 formatting

--- county/models.py
+++ county/models.py
@@ -1,12 +1,24 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""List of classes necessary for views.py"""
+
 # Register your models here.
 from datetime import datetime
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
+# User Class
 class User(AbstractUser):
     pass
 
+# List of Counties for each user 
 class CountyListEntry(models.Model):
-    user=models.ForeignKey('User', on_delete=models.CASCADE, related_name='usernames')
-    countyname=models.CharField(max_length=255)
+    """The following stores data on the list of counties for each user.
+    
+    This class is important for the "My List" functionality of the app.
+    """
+    user=models.ForeignKey('User',
+                           on_delete=models.CASCADE,
+                           related_name='usernames')
+    county_name=models.CharField(max_length=255)
     content = models.CharField(max_length=255)

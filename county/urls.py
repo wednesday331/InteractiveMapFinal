2674ace8b193f3e9cbb97e7c59209f1837419c78
wednesday331 <65116875+wednesday331@users.noreply.github.com>
2020-10-01 09:48:11 -0400

    PEP 8 formatting

--- county/urls.py
+++ county/urls.py
@@ -1,5 +1,8 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""List of Django paths for the map guide code."""
+
 from django.urls import path
-from django.conf.urls import url
 from . import views
 
 urlpatterns = [
@@ -7,8 +10,8 @@ urlpatterns = [
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
-    path("yourlist", views.yourlist, name="yourlist"),
-    path("countyinformation/<str:countyname>", views.countyinformation, name="countyinformation"),
-    path("unsavecounty/<str:countyname>", views.unsavecounty, name="unsavecounty"),
-    path("savecounty/<str:countyname>", views.savecounty, name="savecounty"),
+    path("yourlist", views.your_list, name="your_list"),
+    path("countyinformation/<str:countyname>", views.county_information, name="county_information"),
+    path("unsavecounty/<str:countyname>", views.unsave_county, name="unsave_county"),
+    path("savecounty/<str:countyname>", views.save_county, name="save_county"),
 ]
